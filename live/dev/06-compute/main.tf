b5073676360216c2af245dff72a336008e296036
Vagner Farias <vfarias@gmail.com>
2026-03-10 14:44:01 -0300

    refactor(modules): isolate AWS edge and compute modules

--- live/dev/06-compute/main.tf
+++ live/dev/06-compute/main.tf
@@ -1,5 +1,5 @@
 module "compute" {
-  source = "../../../modules/compute"
+  source = "../../../modules/aws/compute"
 
   infrastructure_name = data.terraform_remote_state.config.outputs.infrastructure_name
   rhcos_ami_id        = data.terraform_remote_state.config.outputs.rhcos_ami_id
